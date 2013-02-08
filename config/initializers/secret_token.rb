2ab62a87b9393a21d4962ec5aed6ca2e04940ec8
Minh Luong <minh.luong@berkeley.edu>
2013-02-08 04:48:55 -0800

    new Rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoginCounter::Application.config.secret_token = 'c4158a3c611130d5810123dd10b18fd8fc0af38ced3a815c3e8a9e47de3bf83c455b26579fa8d83fb79e6b21f6e405d6d039c8688df017d78e929b1ec935f621'
