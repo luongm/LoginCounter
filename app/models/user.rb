366ceb5419bbf8665c90fd6cb0fc667ee3fa4c5d
Minh Luong <minh.luong@berkeley.edu>
2013-02-11 02:45:25 -0800

    Fix: password can be non-empty

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
 	attr_accessible :count, :password, :username
 
 	validates :username, presence: true, length: {maximum: 128}, uniqueness: true
-	validates :password, presence: true, length: {maximum: 128}, on: :create
+	validates :password, length: {maximum: 128}, on: :create
 	# has_secure_password
 end
