fe610c08ba313b9c7b19dc8c3e6c2fb2eac0b81c
Minh Luong <minh.luong@berkeley.edu>
2013-02-17 22:54:23 -0800

    Add ability to render html for UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,75 @@
 class UsersController < ApplicationController
+  # GET /
+  def home
+    @user = User.new
+    
+    if params[:commit] == 'Login'
+      login
+    elsif params[:commit] == 'Add'
+      add
+    else
+      respond_to do |format|
+        format.html # home.html.erb
+      end
+    end
+  end
+
   # POST /users/login
   def login
-    user = User.where("username = ? AND password = ?", params[:user], (params[:password] or ""))[0]
-    if user.nil?
+    @user = User.where("username = ? AND password = ?", params[:user], (params[:password] or ""))[0]
+
+    errCode = 0
+    jsonResult = {}
+    
+    if @user.nil?
       errCode = -1 # ERR_BAD_CREDENTIALS
-      respond_to do |format|
-        format.json { render json: {errCode: errCode} }
-      end
+      flash[:notice] = "Can't login because of invalid credentials!."
     else
-      user.count += 1
-      user.save
       errCode = 1 # SUCCESS
-      respond_to do |format|
-        format.json { render json: {errCode: errCode, count: user.count} }
-      end
+      flash[:notice] = nil
+
+      @user.count += 1
+      @user.save
+      jsonResult[:count] = @user.count
+    end
+
+    jsonResult[:errCode] = errCode
+    respond_to do |format|
+      format.html { render (errCode == 1 ? :profile : :home) }
+      format.json { render json: jsonResult }
     end
   end
 
   # POST /users/add
   def add
-    user = User.new
-    user.username = params[:user]
-    user.password = params[:password]
-    user.count = 1
+    @user = User.new
+    @user.username = params[:user]
+    @user.password = params[:password]
+    @user.count = 1
     
-    errCode = 1 # SUCCESS
-    if user.save
-      respond_to do |format|
-        format.json { render json: {errCode: errCode, count: 1} }
-      end
+    errCode = 0
+    jsonResult = {}
+
+    if @user.save
+      errCode = 1 # SUCCESS
+      flash[:notice] = nil
+
+      jsonResult[:count] = 1
+    elsif @user.username.nil? or @user.username == "" or @user.username.length > 128
+      errCode = -3 # ERR_BAD_USERNAME
+      flash[:notice] = "Can't add user because of bad input username '#{@user.username}'."
+    elsif @user.password.length > 128
+      errCode = -4 # ERR_BAD_PASSWORD
+      flash[:notice] = "Can't add user because of bad input password '#{@user.password}'."
     else
-      if user.username.nil? or user.username == "" or user.username.length > 128
-        errCode = -3 # ERR_BAD_USERNAME
-      elsif user.password.length > 128
-        errCode = -4 # ERR_BAD_PASSWORD
-      else
-        errCode = -2 # ERR_USER_EXISTS
-      end
-      respond_to do |format|
-        format.json { render json: {errCode: errCode} }
-      end
+      errCode = -2 # ERR_USER_EXISTS
+      flash[:notice] = "Can't add user because user '#{@user.username}' already existed."
+    end
+
+    jsonResult[:errCode] = errCode
+    respond_to do |format|
+      format.html { render (errCode == 1 ? :profile : :home) }
+      format.json { render json: jsonResult }
     end
   end
 
